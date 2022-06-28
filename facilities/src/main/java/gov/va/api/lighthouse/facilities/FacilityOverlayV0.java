8eb3708a8b386e1eb353003bde590599482c1477
Chris Bratti <88675604+ChrisBratti-VA@users.noreply.github.com>
2022-06-28 12:38:50 -0400

    Moved the processing of the operating status to reload.

--- facilities/src/main/java/gov/va/api/lighthouse/facilities/FacilityOverlayV0.java
+++ facilities/src/main/java/gov/va/api/lighthouse/facilities/FacilityOverlayV0.java
@@ -5,9 +5,6 @@ import static gov.va.api.lighthouse.facilities.FacilityOverlayHelper.filterOutIn
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.va.api.lighthouse.facilities.api.v0.Facility;
-import gov.va.api.lighthouse.facilities.api.v0.Facility.ActiveStatus;
-import gov.va.api.lighthouse.facilities.api.v0.Facility.OperatingStatus;
-import gov.va.api.lighthouse.facilities.api.v0.Facility.OperatingStatusCode;
 import java.util.function.Function;
 import lombok.Builder;
 import lombok.SneakyThrows;
@@ -19,16 +16,6 @@ public class FacilityOverlayV0 implements Function<HasFacilityPayload, Facility>
 
   private static final ObjectMapper DATAMART_MAPPER = createMapper();
 
-  private static OperatingStatus determineOperatingStatusFromActiveStatus(
-      ActiveStatus activeStatus) {
-    return OperatingStatus.builder()
-        .code(
-            activeStatus == ActiveStatus.T
-                ? OperatingStatusCode.CLOSED
-                : OperatingStatusCode.NORMAL)
-        .build();
-  }
-
   @Override
   @SneakyThrows
   public Facility apply(HasFacilityPayload entity) {
@@ -36,13 +23,6 @@ public class FacilityOverlayV0 implements Function<HasFacilityPayload, Facility>
         FacilityTransformerV0.toFacility(
             filterOutInvalidDetailedServices(
                 DATAMART_MAPPER.readValue(entity.facility(), DatamartFacility.class)));
-
-    if (facility.attributes().operatingStatus() == null) {
-      facility
-          .attributes()
-          .operatingStatus(
-              determineOperatingStatusFromActiveStatus(facility.attributes().activeStatus()));
-    }
     return facility;
   }
 }

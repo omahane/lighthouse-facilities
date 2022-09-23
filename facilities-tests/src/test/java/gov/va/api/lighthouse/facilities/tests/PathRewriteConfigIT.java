32b98a152c6d772dd62ff5037d3205876269814d
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2022-09-23 07:21:24 -0400

    Additional path rewrite testing

--- facilities-tests/src/test/java/gov/va/api/lighthouse/facilities/tests/PathRewriteConfigIT.java
+++ facilities-tests/src/test/java/gov/va/api/lighthouse/facilities/tests/PathRewriteConfigIT.java
@@ -5,37 +5,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import gov.va.api.health.sentinel.ExpectedResponse;
+import gov.va.api.lighthouse.facilities.tests.SystemDefinitions.Service;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.specification.RequestSpecification;
+import java.util.stream.Stream;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @Slf4j
 public class PathRewriteConfigIT {
+  static Stream<Arguments> pathIsRewritten() {
+    return Stream.of(
+        Arguments.of(systemDefinition().facilities()),
+        Arguments.of(systemDefinition().facilitiesInternal()));
+  }
+
   private static RequestSpecification requestSpecification() {
     SystemDefinitions.Service svc = systemDefinition().facilities();
     return RestAssured.given().baseUri(svc.url()).port(svc.port()).relaxedHTTPSValidation();
   }
 
   @ParameterizedTest
-  @ValueSource(
-      strings = {
-        "/facilities/actuator/health",
-        "/va_facilities/actuator/health",
-        "/services/va_facilities/actuator/health"
-      })
-  void pathIsRewritten(String path) {
-    SystemDefinitions.Service svc = systemDefinition().facilities();
+  @MethodSource
+  void pathIsRewritten(Service svc) {
     assertThat(
             ExpectedResponse.of(
                     requestSpecification()
                         .contentType("application/json")
-                        .request(Method.GET, svc.url() + path))
+                        .request(Method.GET, svc.urlWithApiPath() + "actuator/health"))
                 .expect(200)
                 .expectValid(HealthCheckStatus.class)
                 .status())

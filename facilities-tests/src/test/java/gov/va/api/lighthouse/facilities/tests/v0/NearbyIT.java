d3af443686d77f3283d7ef966f42465743f0103f
Chris Bratti <88675604+ChrisBratti-VA@users.noreply.github.com>
2023-04-20 11:42:15 -0400

    Deprecate /nearby search by address

--- facilities-tests/src/test/java/gov/va/api/lighthouse/facilities/tests/v0/NearbyIT.java
+++ facilities-tests/src/test/java/gov/va/api/lighthouse/facilities/tests/v0/NearbyIT.java
@@ -2,13 +2,18 @@ package gov.va.api.lighthouse.facilities.tests.v0;
 
 import static gov.va.api.lighthouse.facilities.tests.FacilitiesRequest.facilitiesRequest;
 import static gov.va.api.lighthouse.facilities.tests.SystemDefinitions.systemDefinition;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import gov.va.api.lighthouse.facilities.api.v0.ApiError;
 import gov.va.api.lighthouse.facilities.api.v0.NearbyResponse;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 public class NearbyIT {
   @Test
   void searchByAddress() {
+    final String errorResponse =
+        "Bing error: Search by address is not supported, please use [lat,lng]";
     final String streetAddress = systemDefinition().ids().streetAddress();
     final String city = systemDefinition().ids().city();
     final String state = systemDefinition().ids().state();
@@ -22,11 +27,63 @@ public class NearbyIT {
             + state
             + "&zip="
             + zip;
-    facilitiesRequest("application/json", request, 200).expectValid(NearbyResponse.class);
+    assertThat(
+            facilitiesRequest("application/json", request, 400)
+                .expectValid(ApiError.class)
+                .errors()
+                .get(0)
+                .detail())
+        .isEqualTo(errorResponse);
+  }
+
+  @Test
+  @SneakyThrows
+  void searchByAddressAndLatLong() {
+    final String streetAddress = systemDefinition().ids().streetAddress();
+    final String city = systemDefinition().ids().city();
+    final String state = systemDefinition().ids().state();
+    final String zip = systemDefinition().ids().zip();
+    final String latitude = systemDefinition().ids().latitude();
+    final String longitude = systemDefinition().ids().longitude();
+    final String searchByAddressFirstRequest =
+        "v0/nearby?street_address="
+            + streetAddress
+            + "&city="
+            + city
+            + "&state="
+            + state
+            + "&zip="
+            + zip
+            + "&drive_time=90"
+            + "&lat="
+            + latitude
+            + "&lng="
+            + longitude;
+    final String searchByLatLongFirstRequest =
+        "v0/nearby?lat="
+            + latitude
+            + "&lng="
+            + longitude
+            + "&street_address="
+            + streetAddress
+            + "&city="
+            + city
+            + "&state="
+            + state
+            + "&zip="
+            + zip
+            + "&drive_time=90";
+    // Verify that search by address parameters are successfully ignored
+    facilitiesRequest("application/json", searchByAddressFirstRequest, 200)
+        .expectValid(NearbyResponse.class);
+    facilitiesRequest("application/json", searchByLatLongFirstRequest, 200)
+        .expectValid(NearbyResponse.class);
   }
 
   @Test
   void searchByAddressWithDriveTime() {
+    final String errorResponse =
+        "Bing error: Search by address is not supported, please use [lat,lng]";
     final String streetAddress = systemDefinition().ids().streetAddress();
     final String city = systemDefinition().ids().city();
     final String state = systemDefinition().ids().state();
@@ -41,7 +98,13 @@ public class NearbyIT {
             + "&zip="
             + zip
             + "&drive_time=90";
-    facilitiesRequest("application/json", request, 200).expectValid(NearbyResponse.class);
+    assertThat(
+            facilitiesRequest("application/json", request, 400)
+                .expectValid(ApiError.class)
+                .errors()
+                .get(0)
+                .detail())
+        .isEqualTo(errorResponse);
   }
 
   @Test
@@ -58,5 +121,20 @@ public class NearbyIT {
     final String longitude = systemDefinition().ids().longitude();
     final String request = "v0/nearby?lat=" + latitude + "&lng=" + longitude + "&drive_time=90";
     facilitiesRequest("application/json", request, 200).expectValid(NearbyResponse.class);
+  }
+
+  @Test
+  @SneakyThrows
+  void searchIncorrectParamsMissingAddress() {
+    final String errorResponse =
+        "Parameter conditions \"lat, lng\" not met for actual request parameters: foo={bar}";
+    final String request = "v0/nearby?foo=bar";
+    assertThat(
+            facilitiesRequest("application/json", request, 400)
+                .expectValid(ApiError.class)
+                .errors()
+                .get(0)
+                .detail())
+        .isEqualTo(errorResponse);
   }
 }
